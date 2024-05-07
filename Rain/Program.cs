5c6c5b1c61cd0d646e911563a8e6b4f4811d2a80
amonrit warawiboon <fourwarawiboon@gmail.com>
2024-05-07 21:08:29 +0700

    update test

--- Rain/Program.cs
+++ Rain/Program.cs
@@ -5,13 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-/*builder.Services.AddControllers();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<DatabaseSetting>(builder.Configuration.GetSection("RainDatabase"));
 builder.Services.AddSingleton<RainService>();
-*/
+
 
 
 // Add services to the container.
@@ -20,11 +20,11 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-/*if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}*/
+}
 
 if (!app.Environment.IsDevelopment())
 {
